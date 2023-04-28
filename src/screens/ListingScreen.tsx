91d91e2fd92a79693541354b8eefab42a3f904eb
thaisclxt <thais.calixto@mail.uft.edu.br>
2023-04-27 23:41:10 -0300

    Fetch local json using axios
    And display breeds names

--- src/screens/ListingScreen.tsx
+++ src/screens/ListingScreen.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Breed {
+	id: number;
+	name: string;
+	temperament: string;
+}
+
 const ListingScreen = () => {
-	return <>Tela de Listagem</>;
+	const [breeds, setBreeds] = useState<Breed[]>([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await axios.get<Breed[]>("breeds.json");
+			setBreeds(response.data);
+		};
+
+		fetchData();
+	}, []);
+	return (
+		<>
+			Tela de Listagem
+			<div>
+				{breeds.map((breed) => (
+					<p key={breed.id}>{breed.name}</p>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default ListingScreen;
