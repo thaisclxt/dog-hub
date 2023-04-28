b3da0a9dd59ad8fc70f48a4ffd076dabcaffef18
thaisclxt <thais.calixto@mail.uft.edu.br>
2023-04-27 22:36:51 -0300

    Create routers Using React Router DOM

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AdoptionScreen from "./screens/AdoptionScreen.tsx";
+import Router from "./router";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<AdoptionScreen />
+		<Router />
 	</React.StrictMode>
 );
---
+++ src/router.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import ListingScreen from "./screens/ListingScreen";
+import AdoptionScreen from "./screens/AdoptionScreen";
+import DetailsScreen from "./screens/DetailsScreen";
+
+const Router = () => (
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<ListingScreen />} />
+			<Route path="/details" element={<DetailsScreen />} />
+			<Route path="/adoption" element={<AdoptionScreen />} />
+		</Routes>
+	</BrowserRouter>
+);
+
+export default Router;
