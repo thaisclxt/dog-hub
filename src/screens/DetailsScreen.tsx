2470f88d3d5e293a177e1d3252a54d69f4324823
thaisclxt <thais.calixto@mail.uft.edu.br>
2023-04-29 16:10:42 -0300

    Fetch breed based on the id for details
    Using axios

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,12 +1,44 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import ScreenTitle from "../components/ScreenTitle";
+
+import axios from "axios";
+
+interface Breed {
+	id: string;
+	name: string;
+	life_span: string;
+	bred_for: string;
+	breed_group: string;
+	temperament: string;
+	weight: {
+		imperial: string;
+		metric: string;
+	};
+	height: {
+		imperial: string;
+		metric: string;
+	};
+}
 
 const DetailsScreen = () => {
 	const { id } = useParams();
+	const [breed, setBreed] = useState<Breed>();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await axios.get<Breed>(`../breed/${id}.json`);
+			setBreed(response.data);
+		};
+
+		fetchData();
+	}, []);
 
 	return (
 		<>
-			<ScreenTitle title="Detalhes" />;
+			<ScreenTitle title="Detalhes" />
+			{id}
+			{breed?.bred_for}
 		</>
 	);
 };
